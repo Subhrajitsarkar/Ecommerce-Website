4ab3bd7861b18b1d6036fdf485f6c0d0bf2b42b3
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2026-01-10 00:44:09 +0530

    Protecting the product page

--- my-ecom-app/src/pages/Store.jsx
+++ my-ecom-app/src/pages/Store.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Products from '../components/Products'
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
+import Products from '../components/Products';
 
 export default function Store() {
+    const { token } = useContext(CartContext);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!token) {
+            history.push('/login');
+        }
+    }, [token, history]);
+
+    if (!token) {
+        return null; // or a loading spinner
+    }
+
     return (
         <section className="page">
             <Products />
         </section>
-    )
+    );
 }
